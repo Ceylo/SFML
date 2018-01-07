bd087cc211060eb52044028aeb99a981445867f7
Ceylo <lucas.soltic@orange.fr>
2018-01-07 14:56:25 +0100

    Remove export dir and move sfml_export_target() in Macros.cmake

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -234,3 +234,29 @@ if(CMAKE_VS_PLATFORM_NAME STREQUAL "Tegra-Android")
         set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
     endmacro()
 endif()
+
+function(sfml_export_targets)
+    # CMAKE_CURRENT_LIST_DIR or CMAKE_CURRENT_SOURCE_DIR not usable for files that are to be included like this one
+    set(CURRENT_DIR "${PROJECT_SOURCE_DIR}/cmake")
+
+    include(CMakePackageConfigHelpers)
+    write_basic_package_version_file("${CMAKE_CURRENT_BINARY_DIR}/SFMLConfigVersion.cmake"
+                                     VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
+                                     COMPATIBILITY SameMajorVersion)
+
+    export(EXPORT SFMLConfigExport
+           FILE "${CMAKE_CURRENT_BINARY_DIR}/SFMLTargets.cmake")
+
+    set(ConfigPackageLocation lib/cmake/SFML)
+    configure_package_config_file("${CURRENT_DIR}/SFMLConfig.cmake.in" "${CMAKE_CURRENT_BINARY_DIR}/SFMLConfig.cmake"
+        INSTALL_DESTINATION "${ConfigPackageLocation}")
+
+    install(EXPORT SFMLConfigExport
+            FILE SFMLTargets.cmake
+            DESTINATION ${ConfigPackageLocation})
+
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/SFMLConfig.cmake" "${CMAKE_CURRENT_BINARY_DIR}/SFMLConfigVersion.cmake"
+            DESTINATION ${ConfigPackageLocation}
+            COMPONENT devel)
+endfunction()
+
