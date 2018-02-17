8b3761db0dc52f1fb5d9407ab57a47af221a48c8
Ceylo <lucas.soltic@orange.fr>
2018-02-17 16:09:06 +0100

    Allow examples to reference the OpenGL interface library

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -283,6 +283,47 @@ if(CMAKE_VS_PLATFORM_NAME STREQUAL "Tegra-Android")
     endmacro()
 endif()
 
+# Create an interface library for an external dependency. This virtual target can provide
+# link specifications and include directories to be used by dependees.
+# Usage: sfml_add_external(target_name
+#                          [INCLUDE "extlibs/include"]
+#                          [LINK "extlibs/libfoo/libfoo.a"])
+function(sfml_add_external)
+    list(GET ARGN 0 target)
+    list(REMOVE_AT ARGN 0)
+
+    if (TARGET ${target})
+        message(FATAL_ERROR "Target '${target}' is already defined")
+    endif()
+
+    cmake_parse_arguments(THIS "" "" "INCLUDE;LINK" ${ARGN})
+    if (THIS_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unknown arguments when calling sfml_import_library: ${THIS_UNPARSED_ARGUMENTS}")
+    endif()
+
+    add_library(${target} INTERFACE)
+
+    if (THIS_INCLUDE)
+        foreach(include_dir IN LISTS "${THIS_INCLUDE}")
+            if (NOT include_dir)
+                message(FATAL_ERROR "No path given for include dir ${THIS_INCLUDE}")
+            endif()
+            target_include_directories(${target} INTERFACE "$<BUILD_INTERFACE:${include_dir}>")
+        endforeach()
+    endif()
+
+    if (THIS_LINK)
+        foreach(link_item IN LISTS ${THIS_LINK})
+            if (NOT link_item)
+                message(FATAL_ERROR "Missing item in ${THIS_LINK}")
+            endif()
+            target_link_libraries(${target} INTERFACE "$<BUILD_INTERFACE:${link_item}>")
+        endforeach()
+    endif()
+
+    install(TARGETS ${target} EXPORT SFMLConfigExport)
+endfunction()
+
 # Find the requested package and make an INTERFACE library from it
 # Usage: sfml_find_package(wanted_target_name
 #                          [INCLUDE "OPENGL_INCLUDE_DIR"]
@@ -307,26 +348,7 @@ function(sfml_find_package)
         find_package(${target} REQUIRED)
     endif()
 
-    add_library(${target} INTERFACE)
-
-    if (THIS_INCLUDE)
-        foreach(include_dir IN LISTS "${THIS_INCLUDE}")
-            if (NOT include_dir)
-                message(FATAL_ERROR "No path given for include dir ${THIS_INCLUDE}")
-            endif()
-            target_include_directories(${target} INTERFACE "$<BUILD_INTERFACE:${include_dir}>")
-        endforeach()
-    endif()
-
-    if (THIS_LINK)
-        foreach(link_item IN LISTS ${THIS_LINK})
-            if (NOT link_item)
-                message(FATAL_ERROR "Missing item in ${THIS_LINK}")
-            endif()
-            target_link_libraries(${target} INTERFACE "$<BUILD_INTERFACE:${link_item}>")
-        endforeach()
-    endif()
-    install(TARGETS ${target} EXPORT SFMLConfigExport)
+    sfml_add_external(${target} INCLUDE ${THIS_INCLUDE} LINK ${THIS_LINK})
 endfunction()
 
 # Generate a SFMLConfig.cmake file (and associated files) from the targets registered against
